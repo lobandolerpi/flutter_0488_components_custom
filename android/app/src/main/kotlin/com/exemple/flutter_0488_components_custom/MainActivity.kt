342e909e98821c79f58f595fdba51de30b584f1a
lobandolerpi <bonilla.p@gmail.com>
2026-02-10 15:59:18 +0100

    flutter create --org com.exemple . i neteja de .gitignore

---
+++ android/app/src/main/kotlin/com/exemple/flutter_0488_components_custom/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.exemple.flutter_0488_components_custom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/iesexemple/flutter_0488_components_custom/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.iesexemple.flutter_0488_components_custom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
